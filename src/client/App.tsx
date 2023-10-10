74c0463d5a7716c74621b647a2c9ef6dc43efbc6
naru-77 <naru.fumi@icloud.com>
2023-10-10 17:53:16 +0900

    App.tsx change

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return <div>Hello from React SSR App!</div>;
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", padding: "20px" }}>
+      <h1>自己紹介</h1>
+      <h2>名前: 山田 太郎</h2>
+      <p>職業: 学生</p>
+      <h3>趣味:</h3>
+      <ul>
+        <li>プログラミング</li>
+        <li>読書</li>
+        <li>旅行</li>
+      </ul>
+      <h3>自己紹介:</h3>
+      <p>こんにちは、山田太郎です。</p>
+    </div>
+  );
 };
 
 export default App;
